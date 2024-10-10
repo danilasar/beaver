5b98a8046b7a221f1ba50199b08a5c05345eab60
danilasar <danila.sar@yandex.ru>
2024-10-10 10:53:36 +0400

    фикс(map) утечки памяти, связанной с бесконечным спавном провинций

--- src/map/mod.rs
+++ src/map/mod.rs
@@ -8,7 +8,6 @@ pub struct Map;
 impl bevy::prelude::Plugin for Map {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Game), systems::setup_map)
-            .add_systems(Update, systems::setup_map.run_if(in_state(GameState::Game)))
             .add_systems(Update, systems::update_button_colors.run_if(in_state(GameState::Game)));
     }
 
